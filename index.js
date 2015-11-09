fe539eff2bbd836a648707bfa117395a1f01edca
Mike James <m.l.james@me.com>
2015-11-09 10:11:11 +0000

    refactoring.

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ try {
 var tribe = require('./lib/tribe-fetcher.js')(config);
 var eventParser = require('./lib/event-parser.js')(config);
 var apiClient = require('./lib/api-client.js')(config);
-
-require('./lib/trigger-client.js')(config);
+var Q = require('q');
 
 console.log('running');
 
@@ -23,11 +22,16 @@ tribe.getApprovedRequest(function(error, data) {
   var events = eventParser(Date.now(), data);
 
   events.forEach(function(event) {
-    apiClient.put(event);
+    apiClient.put(event)
+    .then(function(){
+      console.log('Completed fetch from tribe');
+
+      console.log('Number of Events: ', events.length);
+    })
+    .catch(function(err){
+      console.log('Error - User In tribe may not be a working from home user', event.email);
+      console.log(err);
+    });
   });
 
-  console.log('Completed fetch from tribe');
-
-  console.log('Number of Events: ', events.length);
-  process.exit(0);
 });
